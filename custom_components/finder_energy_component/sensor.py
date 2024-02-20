3271bf3df0ba86b3cd183dd5fac9f38d08589358
Julian Hildemann <j.hildemann@quadrat.gmbh>
2024-02-20 19:59:32 +0100

    first commit

--- custom_components/finder_energy_component/sensor.py
+++ custom_components/finder_energy_component/sensor.py
@@ -1,8 +1,12 @@
-"""GitHub sensor platform."""
 import logging
 import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME, CONF_UNIT_OF_MEASUREMENT, CONF_ENTITY_ID
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_UNIT_OF_MEASUREMENT,
+    CONF_ENTITY_ID,
+    CONF_UNIQUE_ID,
+)
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
@@ -14,6 +18,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default="Finder Measurement Sensor"): cv.string,
     vol.Optional(CONF_UNIT_OF_MEASUREMENT, default="V"): cv.string,
     vol.Required(CONF_ENTITY_ID): cv.entity_id,
+    vol.Optional(CONF_UNIQUE_ID): cv.string,
 })
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
@@ -21,8 +26,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
     unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
     entity_id = config.get(CONF_ENTITY_ID)
+    unique_id = config.get(CONF_UNIQUE_ID)
 
-    sensor = FinderMeasurementSensor(name, unit_of_measurement, entity_id)
+    sensor = FinderMeasurementSensor(
+        name, unit_of_measurement, entity_id, unique_id
+    )
     add_entities([sensor])
 
     def state_changed_listener(event):
@@ -37,12 +45,13 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class FinderMeasurementSensor(Entity):
     """Representation of a Finder Measurement sensor."""
 
-    def __init__(self, name, unit_of_measurement, entity_id):
+    def __init__(self, name, unit_of_measurement, entity_id, unique_id):
         """Initialize the sensor."""
         self._state = None
         self._unit_of_measurement = unit_of_measurement
         self._name = name
         self._entity_id = entity_id
+        self._unique_id = unique_id
 
     @property
     def name(self):
@@ -58,6 +67,11 @@ class FinderMeasurementSensor(Entity):
     def unit_of_measurement(self):
         """Return the unit of measurement."""
         return self._unit_of_measurement
+
+    @property
+    def unique_id(self):
+        """Return a unique identifier for this sensor."""
+        return self._unique_id
 
     def update(self):
         """Update the sensor state."""
@@ -82,16 +96,15 @@ def interpret_measurement(value):
 
         # Replace this with your custom interpretation logic
 
-        hex_value = float(value)  # Assuming the sensor state is in hexadecimal format
-        decade_exponent = (int(hex_value, 16) >> 24) - 1
-        signed_measurement = int(hex_value, 16) & 0xFFFFFF
-        interpreted_value = (signed_measurement * 10 ** decade_exponent)
+        int_value = float(value)  # Assuming the sensor state is in hexadecimal format
+        decade_exponent = 255 - 1 - ((int(int_value)  >> 24) & 0xFF)
+        signed_measurement = int(int_value)  & 0xFFFFFF
+        interpreted_value = signed_measurement * 10 ** decade_exponent
         # return round(interpreted_value, 2)
-        _LOGGER.info("Finder T5 function executed with result: %s", interpreted_value)
-        return interpreted_value
-
-        #interpreted_value = float(value)  # Placeholder logic, assuming the value is numeric
-        #return round(interpreted_value, 2)
+        #_LOGGER.info("Finder T5 function executed with result: %s", round(interpreted_value, 2) )
+        #_LOGGER.info("Finder T5 function executed with decade_exponent: %s", decade_exponent)
+        #_LOGGER.info("Finder T5 function executed with signed_measurement: %s", signed_measurement)
+        return round(interpreted_value, 2)
     except ValueError as ve:
         _LOGGER.error("Error interpreting measurement: %s", str(ve))
         return None
